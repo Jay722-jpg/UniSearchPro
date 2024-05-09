19e620de9b3e07f2e2c83d0056f31e6f251cd551
Jeremiah Locarno <jeremiah.locarano@owl.ucc.edu>
2024-05-08 22:48:05 -0400

    Initial Commit

---
+++ UniSearchPro/UniSearchProApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniSearchProApp.swift
+//  UniSearchPro
+//
+//  Created by Jeremiah Locarno on 5/8/24.
+//
+
+import SwiftUI
+
+@main
+struct UniSearchProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
